37da59bb5fbbee3ec370c1bb5240c5224f86e0d2
Daniel Mejia <webmandman@gmail.com>
2017-03-16 17:42:47 -0700

    02-babel-es6-eslint-flow-jest-husky. Looking good so far. I learned that after cmd yarn test if the end result is error Command failed with exit code 2 that means there was atleast one lint error. phew.

---
+++ src/dog.js
@@ -0,0 +1,16 @@
+// @flow
+/* eslint-disable no-console */
+
+class Dog {
+  name: string
+
+  constructor(name: string) {
+    this.name = name
+  }
+
+  bark() {
+    return `Wah wah, I am ${this.name}`
+  }
+}
+
+export default Dog
